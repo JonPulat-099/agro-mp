8e623677f9c112891514e7a53882c5e565683c47
JonPulat-099 <rjpm023@gmail.com>
2022-09-24 08:36:13 +0500

    Add build dir parameter

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,11 +25,11 @@ export default {
     '~/plugins/mixins.js',
     '~/plugins/axios.js',
   ],
-
+  buildDir: 'dist',
+  ssr: true,
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
   target: 'static',
-  ssr: true,
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
